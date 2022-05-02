5ea9d0586ddd8efd772c8b29e9d89b43be17412a
Avraham Appel <aappel248@gmail.com>
2022-05-02 14:07:54 -0400

    Don't use strings for binary numbers

--- src/binary_diagnostic.rs
+++ src/binary_diagnostic.rs
@@ -2,32 +2,41 @@ pub fn diagnose<I>(data: I) -> u32
 where
     I: Iterator<Item = String>,
 {
-    let data: Vec<Vec<_>> = data.map(|s| s.chars().collect()).collect();
-    let mut gamma = String::with_capacity(data[0].len());
+    let data: Vec<Vec<_>> = data
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(2).expect("Couldn't parse digit"))
+                .collect()
+        })
+        .collect();
+    let mut gamma = Vec::with_capacity(data[0].len());
 
     for i in 0..data[0].len() {
-        let digits_sum: u32 = data
-            .iter()
-            .map(|c| c[i].to_digit(2).expect("Couldn't parse digit"))
-            .sum();
+        let digits_sum: u32 = data.iter().map(|d| d[i]).sum();
 
         let gamma_digit = if digits_sum > data.len() as u32 / 2 {
-            '1'
+            1
         } else {
-            '0'
+            0
         };
 
         gamma.push(gamma_digit);
     }
 
-    let err = "Bin to dec parsing failed";
-    let gamma_dec = u32::from_str_radix(&gamma, 2).expect(err);
-    let epsilon: String = gamma
-        .chars()
-        .map(|b| if b == '1' { '0' } else { '1' })
-        .collect();
+    let epsilon: Vec<_> = gamma.iter().map(|b| if *b == 1 { 0 } else { 1 }).collect();
 
-    gamma_dec * u32::from_str_radix(&epsilon, 2).expect(err)
+    bin_to_dec(&gamma) * bin_to_dec(&epsilon)
+}
+
+fn bin_to_dec(bin: &[u8]) -> u32 {
+    if bin.is_empty() {
+        return 0;
+    }
+
+    bin.into_iter()
+        .map(|u| *u as u32)
+        .reduce(|res, b| (res * 2) + b)
+        .unwrap()
 }
 
 #[cfg(test)]
